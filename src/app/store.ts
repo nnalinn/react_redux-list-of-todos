ea9f1114f8b57889c1ea1c74da2c3cb8b671d356
nnalinn <alina.vysotskayaaa@gmail.com>
2025-04-19 00:42:38 +0300

    add task solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,7 @@ import { combineSlices, configureStore } from '@reduxjs/toolkit';
 import { todosSlice } from '../features/todos';
 import { currentTodoSlice } from '../features/currentTodo';
 import { filterSlice } from '../features/filter';
-import { useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 const rootReducer = combineSlices(todosSlice, currentTodoSlice, filterSlice);
 
@@ -13,4 +13,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
